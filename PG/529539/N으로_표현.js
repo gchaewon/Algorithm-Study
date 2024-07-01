774c9b254af4cdb42a7b1981b6f7f13dd33ab963
서진 <dltjwls0529@gmail.com>
2024-07-01 15:35:38 +0200

    [chore] 환경 세팅 및 테스트

---
+++ PG/529539/N으로_표현.js
@@ -0,0 +1,13 @@
+// 2024.07.05 풀이
+// N으로 표현
+// https://school.programmers.co.kr/learn/courses/30/lessons/42895
+
+const N = 5;
+const number = 12;
+
+function solution(N, number) {
+	let answer = 0;
+	return answer;
+}
+
+console.log(solution(N, number));
