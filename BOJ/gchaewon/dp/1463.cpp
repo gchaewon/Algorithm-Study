f0ce88a04bb1247b01defab985887bcd6e7b1fb3
gchaewon <conjs1@naver.com>
2024-07-04 16:00:08 +0900

    [1463] 1로 만들기

--- BOJ/gchaewon/dp/1463.cpp
+++ BOJ/gchaewon/dp/1463.cpp
@@ -11,14 +11,12 @@ int main() {
     dp[1] = 0;
 
     for (int i = 2; i <= n; i++) {
-        if (i % 3 == 0 && i % 2 == 0) {
-            dp[i] = min(dp[i / 3] + 1, min(dp[i / 2] + 1, dp[i - 1] + 1));
-        } else if (i % 3 == 0) {
-            dp[i] = min(dp[i / 3] + 1, dp[i - 1] + 1);
-        } else if (i % 2 == 0) {
-            dp[i] = min(dp[i / 2] + 1, dp[i - 1] + 1);
-        } else {
-            dp[i] = dp[i - 1] + 1;
+        dp[i] = dp[i - 1] + 1;
+        if (i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        if (i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
         }
     }
     cout << dp[n];
