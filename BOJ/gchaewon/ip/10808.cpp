215e8d77f082f9badb49f29c2c8a505de8d7c223
gchaewon <conjs1@naver.com>
2024-11-04 15:43:39 +0900

    [10808] 알파벳 개수

---
+++ BOJ/gchaewon/ip/10808.cpp
@@ -0,0 +1,20 @@
+// 알파벳 개수 카운팅
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string str;
+    vector<int> v(26, 0);
+    cin >> str;
+
+    for (auto i : str) {
+        v[i - 'a']++;
+    }
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    return 0;
+}
