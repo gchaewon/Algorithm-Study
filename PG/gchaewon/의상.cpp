a45bd80cc223f4a8fa1c4ee7f47832254e7394b7
gchaewon <conjs1@naver.com>
2025-02-11 23:14:20 +0900

    [PG] 의상

--- PG/gchaewon/의상.cpp
+++ PG/gchaewon/의상.cpp
@@ -1,27 +1,22 @@
-#include <iostream>
-#include <map>
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 1;
-    map<string, int> cnt;
+    int ans = 1;
+    map<string, int> m; // 카테고리별 옷 가지수 저장
 
     for (auto cloth : clothes) {
-        if (cnt.find(cloth[1]) != cnt.end()) {
-            cnt[cloth[1]]++;
+        if (m.find(cloth[1]) != m.end()) { // 등록되어 있는 경우, 숫자만 증가
+            m[cloth[1]]++;
         } else {
-            cnt.insert({cloth[1], 1});
+            m.insert({cloth[1], 1}); // 없는 경우, 새로 넣기
         }
     }
 
-    for (auto i : cnt) {
-        answer *= (i.second + 1);
+    for (auto i : m) {
+        ans *= (i.second + 1); // 옷 가지수 + 선택하지 않는 경우 1
     }
+    ans -= 1; // 아무것도 입지 않는 경우 제외
 
-    answer -= 1;
-
-    return answer;
+    return ans;
 }
