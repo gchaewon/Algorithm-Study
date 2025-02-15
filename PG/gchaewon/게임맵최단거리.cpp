3707ac662d02af5844acf7536574c738fa9fca99
gchaewon <conjs1@naver.com>
2025-02-15 17:09:43 +0900

    [PG] 게임맵최단거리

--- PG/gchaewon/게임맵최단거리.cpp
+++ PG/gchaewon/게임맵최단거리.cpp
@@ -1,60 +1,50 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-
+#include <bits/stdc++.h>
 #define X first
 #define Y second
-
 using namespace std;
 
-int n, m;
-vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int visited[104][104];
+vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-int bfs(vector<vector<int>> &maps) {
-    vector<vector<int>> dist(n, vector<int>(m, 0));
+void bfs(vector<vector<int>> maps, int n, int m) {
     queue<pair<int, int>> q;
     q.push({0, 0});
-    maps[0][0] = -1; // 방문처리;
-    dist[0][0] = 1;
 
     while (!q.empty()) {
         int x = q.front().X;
         int y = q.front().Y;
-
         q.pop();
-        maps[x][y] = -1;
-
         if (x == n - 1 && y == m - 1) {
-            return dist[x][y];
+            return;
         }
 
         for (auto d : dir) {
             int dx = x + d.X;
             int dy = y + d.Y;
 
-            if (dx < 0 || dx >= n || dy < 0 || dy >= m) {
+            if (dx < 0 || dx >= n || dy < 0 || dy >= m || !maps[dx][dy] ||
+                visited[dx][dy]) {
                 continue;
             }
-
-            if (maps[dx][dy] != 1) {
-                continue;
-            }
-            // 큐에 넣기 전 방문처리하여, for문 수행 중 위치가 중복으로 들어가지
-            // 않도록 함 (효율성)
-            maps[dx][dy] = -1;
-            dist[dx][dy] = dist[x][y] + 1;
+            visited[dx][dy] = visited[x][y] + 1;
             q.push({dx, dy});
         }
     }
-    return -1;
 }
-
 int solution(vector<vector<int>> maps) {
-    int answer = 0;
-    n = maps.size();
-    m = maps[0].size();
+    int n = maps.size();
+    int m = maps[0].size();
 
-    answer = bfs(maps);
+    int ans = 0;
 
-    return answer;
+    visited[0][0] = 1;
+    bfs(maps, n, m);
+
+    if (!visited[n - 1][m - 1]) {
+        ans = -1;
+    } else {
+        ans = visited[n - 1][m - 1];
+    }
+
+    return ans;
 }
