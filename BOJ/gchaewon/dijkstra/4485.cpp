1de2c31b96db16e12ba5de65522964f13e53cb82
gchaewon <conjs1@naver.com>
2025-03-19 16:39:02 +0900

    [4485] 녹색 옷 입은 애가 젤다지? (정렬 수정)

--- BOJ/gchaewon/dijkstra/4485.cpp
+++ BOJ/gchaewon/dijkstra/4485.cpp
@@ -5,8 +5,6 @@
 #include <queue>
 #include <vector>
 
-#define X first
-#define Y second
 using namespace std;
 
 int n;
@@ -15,19 +13,28 @@ int a[129][129], dist[129][129];
 vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
 void dijkstra() {
-    priority_queue<pair<int, int>> pq;
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                   greater<>>
+        pq;
+    // {비용,{x, y}} 비용 오름차순
 
     dist[0][0] = a[0][0];
-    pq.push({0, 0});
+    pq.push({a[0][0], {0, 0}});
 
     while (!pq.empty()) {
-        int x = pq.top().X;
-        int y = pq.top().Y;
+        int x = pq.top().second.first;
+        int y = pq.top().second.second;
+        int cur_cost = pq.top().first;
         pq.pop();
 
+        // 기존 경로값보다 큰 값이면 패스
+        if (dist[x][y] < cur_cost) {
+            continue;
+        }
+
         for (auto d : dir) {
-            int dx = x + d.X;
-            int dy = y + d.Y;
+            int dx = x + d.first;
+            int dy = y + d.second;
 
             if (dx < 0 || dx >= n || dy < 0 || dy >= n) {
                 continue;
@@ -35,7 +42,7 @@ void dijkstra() {
 
             if (dist[x][y] + a[dx][dy] < dist[dx][dy]) {
                 dist[dx][dy] = dist[x][y] + a[dx][dy];
-                pq.push({dx, dy});
+                pq.push({dist[dx][dy], {dx, dy}});
             }
         }
     }
