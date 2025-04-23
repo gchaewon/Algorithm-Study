0982dcd15894b733535084d2de15824f684be21b
gchaewon <conjs1@naver.com>
2025-04-23 22:32:32 +0900

    [11055] 가장 큰 증가하는 부분 수열

--- BOJ/gchaewon/dp/11055.cpp
+++ BOJ/gchaewon/dp/11055.cpp
@@ -1,29 +1,28 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
 int main() {
     int n;
-    int answer = 0;
     cin >> n;
-    vector<int> arr(n + 1, 0);
-    vector<int> dp(n + 1,
-                   0); // dp[i] = arr[i]를 포함하는 arr[i]보다 작은 원소의 총합
+    vector<int> a(n + 4, 0);
+    vector<int> dp(n + 4, 0); // i번째 원소를 포함하는 최대 증가 부분 수열의 합
 
     for (int i = 0; i < n; i++) {
-        cin >> arr[i + 1];
+        cin >> a[i];
     }
-    dp[1] = arr[1];
 
-    for (int i = 2; i <= n; i++) {
-        // do[i] = arr[i]보다 작은 원소 arr[j] 중 가장 큰 dp[j] + arr[i]
-        for (int j = i - 1; j >= 1; j--) {
-            if (arr[j] < arr[i]) {
-                dp[i] = max(dp[i], dp[j]);
+    dp[n - 1] = a[n - 1];
+
+    for (int i = n; i >= 0; i--) {
+        dp[i] = a[i];
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] < a[j]) {
+                dp[i] = max(dp[i], dp[j] + a[i]);
             }
         }
-        dp[i] += arr[i];
     }
 
     sort(dp.rbegin(), dp.rend());
