8f694c39dc44e92e025a4f5c146f6a12031ff071
gchaewon <conjs1@naver.com>
2025-05-02 23:31:48 +0900

    [17219] 비밀번호 찾기 (unordered_map)

--- BOJ/gchaewon/hash/17219.cpp
+++ BOJ/gchaewon/hash/17219.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 int main() {
     int n, m;
     string site, name;
-    map<string, string> address;
+    unordered_map<string, string> address;
     cin >> n >> m;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     for (int i = 0; i < n; i++) {
         cin >> site >> name;
-        address[site] = name;
+        address.insert({site, name});
     }
 
     for (int i = 0; i < m; i++) {
