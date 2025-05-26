5559b3d83ad020387e5ed89c0b9d7c6f9b516fc8
gchaewon <conjs1@naver.com>
2025-05-26 16:34:32 +0900

    [PG] 단어 변환

--- PG/gchaewon/단어변환.cpp
+++ PG/gchaewon/단어변환.cpp
@@ -1,64 +1,70 @@
 #include <iostream>
-#include <queue>
 #include <set>
 #include <string>
 #include <vector>
 
 using namespace std;
+int answer = 54;
+int visited[54];
 
-int solution(string begin, string target, vector<string> v) {
-    int answer = 0;
-    string pre;
-    int n = begin.length();
-    vector<bool> is_finished(n, false); // n자리 알파벳이 타겟과 일치하는지 여부
-    set<string> words;
-    queue<string> q;
-
-    // 단어 삭제를 편리하게 하기 위해, set 사용
-    for (auto word : v) {
-        words.insert(word);
-    }
-
-    q.push(begin);
-
-    while (words.size() >= 0 && !q.empty()) {
-        pre = q.front();
-        q.pop();
-
-        if (pre == target) {
-            return answer;
-        }
-
-        for (auto word : words) {
-            int diff = 0;
-            int change = 0;
-            for (int i = 0; i < n; i++) {
-                // 알파벳 차이가 2개 이상이라면 (다음 단어로 변경 불가)
-                if (diff >= 2) {
-                    break;
-                }
-                if (word[i] != pre[i]) {
-                    diff++;
-                    change = i;
-                }
-            }
-            // 알파벳 차이가 1개일 때
-            if (diff == 1) {
-                // 이미 완료한 자리는 바꾸지 않음
-                if (is_finished[change]) {
-                    break;
-                }
-                // 타겟 자리와 같은 자리라면 처리
-                if (target[change] == word[change]) {
-                    is_finished[change] = true;
-                }
-                q.push(word);
-                words.erase(word);
-                answer++;
-                break;
+bool checkValid(string cur, string next) {
+    int cnt = 0;
+    // 현재 단어에서 1개 알파벳보다 많이 차이나는지 확인
+    for (int i = 0; i < cur.size(); i++) {
+        if (cur[i] != next[i]) {
+            cnt++;
+            // 1개 이상 차이나면 변환 불가
+            if (cnt > 1) {
+                return false;
             }
         }
     }
+    return true;
+}
 
-    return 0;
+void backTracking(string cur, string target, vector<string> words, int depth) {
+    // 최단이 아닌 경우 제외
+    if (depth > answer) {
+        return;
+    }
+    // 타겟 단어와 같으면 종료
+    if (cur == target) {
+        answer = depth;
+        return;
+    }
+
+    for (int i = 0; i < words.size(); i++) {
+        // 이미 탐색한 단어면 제외
+        if (visited[i]) {
+            continue;
+        }
+        // 알파벳 1개 이상 차이나는 경우 제외
+        if (!checkValid(cur, words[i])) {
+            continue;
+        }
+        // 방문처리
+        visited[i] = 1;
+        // 탐색
+        backTracking(words[i], target, words, depth + 1);
+        // 원복
+        visited[i] = 0;
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+
+    set<string> s;
+
+    for (auto word : words) {
+        s.insert(word);
+    }
+
+    // 단어 집합에 타겟 단어가 없는 경우 종료
+    if (s.find(target) == s.end()) {
+        return 0;
+    }
+
+    backTracking(begin, target, words, 0);
+
+    return answer;
 }
