220c553634fcc9131f6d907f9e4f2c503ed01a7b
gchaewon <conjs1@naver.com>
2025-05-27 16:33:19 +0900

    [PG] 같은 숫자는 싫어

---
+++ PG/gchaewon/같은숫자는싫어.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    stack<int> s;
+    vector<int> answer;
+
+    s.push(arr.front());
+
+    for (int i = 1; i < arr.size(); i++) {
+        // 중복인 경우 추가하지 않음
+        if (s.top() == arr[i]) {
+            continue;
+        }
+        s.push(arr[i]);
+    }
+
+    // 원래 순서대로 출력하기 위해 뒤집음
+    while (!s.empty()) {
+        answer.push_back(s.top());
+        s.pop();
+    }
+    reverse(answer.begin(), answer.end());
+
+    return answer;
+}
