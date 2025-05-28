8730d9674e410056fd46edfaafd2427ee4b952b7
gchaewon <conjs1@naver.com>
2025-05-28 17:44:31 +0900

    [PG] 여행경로

--- PG/gchaewon/여행경로.cpp
+++ PG/gchaewon/여행경로.cpp
@@ -1,34 +1,55 @@
 #include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
 #include <vector>
-
 using namespace std;
 
-vector<int> visited(10001, 0);
-vector<string> answer;
+int n;
+map<string, vector<pair<string, bool>>>
+    infos; // {시작, {도착 공항 정보, 사용여부}}
+vector<string> path;
+set<vector<string>> paths;
 
-bool dfs(int n, string cur, vector<vector<string>> tickets) {
-    if (n == tickets.size()) {
-        return true;
+void dfs(string cur, int cnt) {
+    // 티켓 다 쓴 경우, 경로 추가 후 종료
+    if (cnt == n) {
+        paths.insert(path);
+        return;
     }
 
-    for (int i = 0; i < tickets.size(); i++) {
-        if (tickets[i][0] == cur && !visited[i]) {
-            visited[i] = 1;
-            answer.push_back(tickets[i][1]);
-            if (dfs(n + 1, tickets[i][1], tickets)) {
-                return true;
-            }
-            // back tracking
-            visited[i] = 0;
-            answer.pop_back();
+    // 현재 공항에서 갈 수 있는 경로 탐색
+    for (auto &next : infos[cur]) {
+        // 사용한 티켓의 경우 제외
+        if (next.second) {
+            continue;
         }
+        // 방문처리
+        next.second = true;
+        path.push_back(next.first);
+
+        // 탐색
+        dfs(next.first, cnt + 1);
+
+        // 원복
+        path.pop_back();
+        next.second = false;
     }
-    return false;
 }
 vector<string> solution(vector<vector<string>> tickets) {
-    sort(tickets.begin(), tickets.end());
-    answer.push_back("ICN");
-    dfs(0, "ICN", tickets);
+    vector<string> answer;
+    n = tickets.size();
 
+    // map에 티켓 정보 저장
+    for (auto ticket : tickets) {
+        infos[ticket[0]].push_back({ticket[1], false});
+    }
+
+    path.push_back("ICN");
+    dfs("ICN", 0);
+
+    // 사전 순으로 앞선 경로 리턴
+    answer = *paths.begin();
     return answer;
 }
