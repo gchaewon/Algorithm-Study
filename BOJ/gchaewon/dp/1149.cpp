48d5eb878748c6c9d31196d6ca34ec2d92f88fca
gchaewon <conjs1@naver.com>
2025-06-06 18:40:36 +0900

    [1149] RGB거리

--- BOJ/gchaewon/dp/1149.cpp
+++ BOJ/gchaewon/dp/1149.cpp
@@ -1,21 +1,28 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector<vector<int>> dp(n + 1, {0, 0, 0});
+    vector<vector<int>> dp(n, vector<int>(3, 0));
+    // dp[i][j] = i번째 집에서 j번째 색으로 칠했을 때 최소 비용
 
-    for (int i = 1; i <= n; i++) {
-        cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> dp[i][j];
+        }
+    }
 
-        // 이전 값과 다른 색을 칠할 때 최소값, 경우에 따라 구하기
+    // 이전 집과 색 겹치지 않는 색 중 최솟값으로 더하기
+    for (int i = 1; i < n; i++) {
         dp[i][0] += min(dp[i - 1][1], dp[i - 1][2]);
         dp[i][1] += min(dp[i - 1][0], dp[i - 1][2]);
         dp[i][2] += min(dp[i - 1][0], dp[i - 1][1]);
     }
 
-    cout << min(dp[n][0], min(dp[n][1], dp[n][2]));
+    sort(dp[n - 1].begin(), dp[n - 1].end());
+    cout << dp[n - 1][0];
+    return 0;
 }
