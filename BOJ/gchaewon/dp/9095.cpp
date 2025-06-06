be06754fde45df6ab80e52af6f83ffc2726d3049
gchaewon <conjs1@naver.com>
2025-06-06 18:03:04 +0900

    [9095] 1, 2, 3 더하기

--- BOJ/gchaewon/dp/9095.cpp
+++ BOJ/gchaewon/dp/9095.cpp
@@ -4,20 +4,19 @@
 using namespace std;
 
 int main() {
-    int t;
+    int t, n;
+    vector<int> dp(12, 0);
     cin >> t;
-    vector<int> dp(11, 0);
 
-    dp[1] = 1; // 1
-    dp[2] = 2; // 1+1, 2
-    dp[3] = 4; // 1+1+1, 2+1, 1+2, 3
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
 
-    for (int i = 4; i <= 10; i++) {
+    for (int i = 4; i < 12; i++) {
         dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
     }
 
     while (t--) {
-        int n;
         cin >> n;
         cout << dp[n] << "\n";
     }
