8e4fde5257067212afb03e24f87d7fa2fa91b48d
gchaewon <conjs1@naver.com>
2025-06-07 17:47:50 +0900

    [11727] 2xn 타일링

--- BOJ/gchaewon/dp/11727.cpp
+++ BOJ/gchaewon/dp/11727.cpp
@@ -2,25 +2,21 @@
 #include <vector>
 
 using namespace std;
+const int MOD = 10007;
 
 int main() {
     int n;
+
     cin >> n;
-    vector<int> dp(n + 1, 0); // dp[i] = i 크기 직사각형을 채우는 방법 수
+    vector<long long> dp(n + 1, 0);
 
     dp[1] = 1;
-    dp[2] = 3;
-    dp[3] = 5;
+    dp[2] = 2;
 
-    for (int i = 4; i <= n; i++) {
-        dp[i] = dp[i - 1] * 2 - 1;
-        if (i % 2 == 0) {
-            dp[i] += 2;
-        }
-        dp[i] %= 10007;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
     }
 
     cout << dp[n];
-
     return 0;
 }
