d36f43732bd86f3ebd5b10e4771eabfbaff396fe
gchaewon <conjs1@naver.com>
2025-06-07 18:12:31 +0900

    [11659] 구간 합 구하기 4

--- BOJ/gchaewon/dp/11659.cpp
+++ BOJ/gchaewon/dp/11659.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
-    int n, m;
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
 
+    int n, m;
     cin >> n >> m;
-    vector<int> v(n + 1, 0);
-    vector<int> dp(n + 1, 0);
+    vector<int> num(n + 1, 0);
+    vector<int> dp(n + 1, 0); // dp[n] = n까지의 누적합
 
     for (int i = 1; i <= n; i++) {
-        cin >> v[i];
-    }
-
-    for (int i = 1; i <= n; i++) {
-        dp[i] = dp[i - 1] + v[i];
+        cin >> num[i];
+        dp[i] = dp[i - 1] + num[i];
     }
 
     while (m--) {
@@ -25,4 +22,5 @@ int main() {
         cin >> i >> j;
         cout << dp[j] - dp[i - 1] << "\n";
     }
+    return 0;
 }
