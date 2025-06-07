cc06e983eaa1820577dc661358a762206b2d0b37
gchaewon <conjs1@naver.com>
2025-06-07 18:43:25 +0900

    [12852] 1로 만들기 2

--- BOJ/gchaewon/dp/12852.cpp
+++ BOJ/gchaewon/dp/12852.cpp
@@ -1,42 +1,42 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> dp(n + 1, 0);
+    int x;
+    cin >> x;
+    vector<long long> dp(x + 3, 0);
+    vector<vector<int>> nums(x + 3, vector<int>());
 
     dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    nums[1] = {1};
+    nums[2] = {2, 1};
+    nums[3] = {3, 1};
 
-    for (int i = 2; i <= n; i++) {
+    for (int i = 4; i <= x; i++) {
         dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0 && i % 3 == 0) {
-            dp[i] = min(dp[i], min(dp[i / 2], dp[i / 3]) + 1);
-        } else if (i % 3 == 0) {
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-        } else if (i % 2 == 0) {
-            dp[i] = min(dp[i], dp[i / 2] + 1);
+        nums[i] = nums[i - 1];
+
+        if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+            dp[i] = dp[i / 2] + 1;
+            nums[i] = nums[i / 2];
         }
+        if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+            dp[i] = dp[i / 3] + 1;
+            nums[i] = nums[i / 3];
+        }
+        nums[i].push_back(i);
     }
 
-    cout << dp[n] << "\n";
+    cout << dp[x] << "\n";
 
-    // 역순으로 출력하면서 n 값 줄이기
-    while (n > 0) {
-        cout << n << " ";
-        if (n == 1) {
-            break;
-        }
-        if (dp[n] == dp[n - 1] + 1) {
-            n--;
-        } else if ((n % 3 == 0) && (dp[n] == dp[n / 3] + 1)) {
-            n /= 3;
-        } else if ((n % 2 == 0) && (dp[n] == dp[n / 2] + 1)) {
-            n /= 2;
-        }
+    sort(nums[x].rbegin(), nums[x].rend());
+    for (auto num : nums[x]) {
+        cout << num << " ";
     }
+
     return 0;
 }
