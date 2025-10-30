d7b7744b581f0b95b11837afef5a134f493263c8
gchaewon <conjs1@naver.com>
2025-10-30 20:33:30 +0900

    [15656] N과 M (7)

---
+++ BOJ/gchaewon/backTracking/15656.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> arr, nums;
+
+void dfs(int cnt) {
+    if (cnt == m) {
+        for (auto a : arr) {
+            cout << a << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (auto num : nums) {
+        arr.push_back(num);
+        dfs(cnt + 1);
+        arr.pop_back();
+    }
+}
+int main() {
+    cin >> n >> m;
+
+    int num;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        nums.push_back(num);
+    }
+    sort(nums.begin(), nums.end());
+    dfs(0);
+
+    return 0;
+}
