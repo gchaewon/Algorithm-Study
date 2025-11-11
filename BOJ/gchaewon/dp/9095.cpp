b704f2a0bcbcc7dcd8f8cc9e2b2cfd5ecbec23fd
gchaewon <conjs1@naver.com>
2025-11-11 17:19:59 +0900

    [9095] 1, 2, 3 더하기

--- BOJ/gchaewon/dp/9095.cpp
+++ BOJ/gchaewon/dp/9095.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
     int t, n;
-    vector<int> dp(12, 0);
+    int max_num = 0;
     cin >> t;
+    vector<int> arr;
+
+    while (t--) {
+        cin >> n;
+        arr.push_back(n);
+        max_num = max(max_num, n);
+    }
+    vector<int> dp(max_num + 4, 0);
 
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
 
-    for (int i = 4; i < 12; i++) {
-        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+    for (int i = 4; i <= max_num; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
     }
 
-    while (t--) {
-        cin >> n;
-        cout << dp[n] << "\n";
+    for (int a : arr) {
+        cout << dp[a] << "\n";
     }
-
     return 0;
 }
